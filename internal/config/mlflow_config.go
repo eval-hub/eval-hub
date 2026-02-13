4d322963fd47a0d123f153332b238ea9685f9242
Rui Vieira <ruidevieira@googlemail.com>
2026-02-13 18:39:54 +0000

    fix(config): remove unused Secure field from MLFlowConfig

--- internal/config/mlflow_config.go
+++ internal/config/mlflow_config.go
@@ -8,7 +8,6 @@ import (
 type MLFlowConfig struct {
 	TrackingURI        string        `mapstructure:"tracking_uri"`
 	HTTPTimeout        time.Duration `mapstructure:"http_timeout"`
-	Secure             bool          `mapstructure:"secure"`
 	CACertPath         string        `mapstructure:"ca_cert_path"`
 	InsecureSkipVerify bool          `mapstructure:"insecure_skip_verify"`
 	TokenPath          string        `mapstructure:"token_path"`
