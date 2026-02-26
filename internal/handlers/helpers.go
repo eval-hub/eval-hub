acbb018a45b3be826b86d6e4ae0d852cfbe1dadb
marius.danciu <marius.danciu@gmail.com>
2026-02-26 10:51:03 +0200

    added type fo validation msg.

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -77,14 +77,14 @@ func CommonListFilters(r http_wrappers.RequestWrapper) (*abstractions.QueryFilte
 		return nil, err
 	}
 	if limit < 0 {
-		return nil, serviceerrors.NewServiceError(messages.QueryParameterInvalid, "ParameterName", "limit", "Value", strconv.Itoa(limit))
+		return nil, serviceerrors.NewServiceError(messages.QueryParameterInvalid, "ParameterName", "limit", "Type", "integer", "Value", strconv.Itoa(limit))
 	}
 	offset, err := GetParam(r, "offset", true, 0)
 	if err != nil {
 		return nil, err
 	}
 	if offset < 0 {
-		return nil, serviceerrors.NewServiceError(messages.QueryParameterInvalid, "ParameterName", "offset", "Value", strconv.Itoa(offset))
+		return nil, serviceerrors.NewServiceError(messages.QueryParameterInvalid, "ParameterName", "offset", "Type", "integer", "Value", strconv.Itoa(offset))
 	}
 	status, err := GetParam(r, "status", true, "")
 	if err != nil {
