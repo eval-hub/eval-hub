8153194306f47b9b6ae302479ecc03c8cfb5cba6
Julian Payne <julpayne@redhat.com>
2026-03-10 14:27:02 +0100

    fix: keep reason in log

--- cmd/eval_hub/server/authorization.go
+++ cmd/eval_hub/server/authorization.go
@@ -42,7 +42,7 @@ func WithAuthorization(next http.Handler, logger *slog.Logger, client *kubernete
 		switch decision {
 
 		case authorizer.DecisionNoOpinion:
-			logger.Error("Unable to authorize request", "path", r.URL.Path, "method", r.Method, "reason", reason, "error", err)
+			logger.Error("Unable to authorize request", "path", r.URL.Path, "method", r.Method, "reason", reason)
 			writeError(w, messages.UnableToAuthorizeRequest, "Error", reason)
 			return
 		case authorizer.DecisionDeny:
