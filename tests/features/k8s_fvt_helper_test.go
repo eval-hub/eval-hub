b4111a25f0e7f82134d301cbb038a92a71944022
nbs-rh <nbs@redhat.com>
2026-06-22 12:26:42 +0530

    test: prefer kubeconfig if present over in-cluster
    Signed-off-by: nbs-rh <nbs@redhat.com>

---
+++ tests/features/k8s_fvt_helper_test.go
@@ -0,0 +1,45 @@
+package features
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestLoadFVTKubeConfigPrefersKubeconfig(t *testing.T) {
+	t.Parallel()
+
+	dir := t.TempDir()
+	kubeconfig := filepath.Join(dir, "config")
+	if err := os.WriteFile(kubeconfig, []byte(minimalKubeconfig), 0o600); err != nil {
+		t.Fatalf("write kubeconfig: %v", err)
+	}
+
+	t.Setenv("KUBECONFIG", kubeconfig)
+
+	cfg, err := loadFVTKubeConfig()
+	if err != nil {
+		t.Fatalf("loadFVTKubeConfig() error = %v", err)
+	}
+	if cfg.Host != "https://test-cluster.example:6443" {
+		t.Fatalf("Host = %q, want https://test-cluster.example:6443", cfg.Host)
+	}
+}
+
+const minimalKubeconfig = `apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    server: https://test-cluster.example:6443
+  name: test
+contexts:
+- context:
+    cluster: test
+    user: test
+  name: test
+current-context: test
+users:
+- name: test
+  user:
+    token: test-token
+`
